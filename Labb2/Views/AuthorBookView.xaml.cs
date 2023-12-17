700b01e7e1d91b68a73656d9f039c9c481262a65
FiggeAhlberg <ahlbergfigge@gmail.com>
2023-12-17 17:10:44 +0100

    Update

--- Labb2/Views/AuthorBookView.xaml.cs
+++ Labb2/Views/AuthorBookView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Labb2.Views
 {
@@ -20,9 +23,168 @@ namespace Labb2.Views
     /// </summary>
     public partial class AuthorBookView : UserControl
     {
+        public Author SelectedAuthor {get; set; }
+
         public AuthorBookView()
         {
             InitializeComponent();
+            LoadBooks();
+            LoadAuthors();
+            LoadPublisher();
+        }
+
+        public void LoadBooks()
+        {
+            EditBooksListView.Items.Clear();
+            var db = new BookstoreContext();
+            var loadBooks = db.Books.ToList();
+            foreach (var book in loadBooks)
+            {
+                EditBooksListView.Items.Add(book);
+            }
+
+        }
+
+        public void LoadAuthors()
+        {
+            var db = new BookstoreContext();
+            var loadAuthors = db.Authors.ToList();
+            foreach (var author in loadAuthors)
+            {
+                EditAuthorListView.Items.Add(author);
+            }
+
+        }
+
+        public void LoadPublisher()
+        {
+
+            using var db = new BookstoreContext();
+
+            var publishers = db.Publishers.ToList();
+
+            foreach (var publisher in publishers)
+            {
+                PublisherCmb.Items.Add(publisher);
+            }
+        }
+        private List<Publisher> GetPublisher(BookstoreContext db)
+        {
+
+            var publisherNewBook = new List<Publisher>();
+
+
+            foreach (var item in PublisherCmb.Items)
+            {
+                if (item is Publisher publisherItem)
+                {
+                    var existingPublisher = db.Publishers.Find(publisherItem.Id);
+                    if (existingPublisher != null)
+                    {
+                      publisherNewBook.Add(existingPublisher); 
+                    }
+                } 
+            }
+
+            return publisherNewBook;
+        }
+
+        private void EditAuthorListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedAuthor = EditAuthorListView.SelectedItem as Author;
+        }
+
+        private void AuthorToBookBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var db = new BookstoreContext();
+
+            AuthorListView.Items.Add(SelectedAuthor);
+        }
+
+        private List<Author> GetAuthor(BookstoreContext db)
+        {
+            var authorNewBook = new List<Author>();
+
+            foreach (var item in AuthorListView.Items)
+            {
+                if (item is Author authorItem)
+                {
+                    var existingAuthor = db.Authors.Find(authorItem.Id);
+                    if (existingAuthor != null)
+                    {
+                        authorNewBook.Add(existingAuthor);
+                    }
+                }
+            }
+            return authorNewBook;
+        }
+
+        private void AddStockBookBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var db = new BookstoreContext();
+
+            if (DateOnly.TryParse(BookReleaseDate.Text, out var isBookReleaseDate))
+            {
+
+                var authorNewBook = GetAuthor(db);
+                var publisherNewBook = GetPublisher(db);
+                db.Books.Add
+                (
+                    new Book()
+                    {
+                        Isbn13 = BookIdTbx.Text,
+                        Title = BookTitleTbx.Text,
+                        Language = BookLanguageTbx.Text,
+                        Price = int.Parse(BookPriceTbx.Text),
+                        Publishers = publisherNewBook,
+                        ReleaseDate = isBookReleaseDate,
+                        Authors = authorNewBook
+
+                    }
+                );
+
+
+                db.SaveChanges();
+                LoadBooks();
+
+                
+            }
+        }
+
+        private void AddAuthorBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DateOnly.TryParse(AuthorBirthDate.Text, out var isAuthorBirthDate))
+            {
+                using var db = new BookstoreContext();
+                db.Authors.Add
+                (
+                    new Author()
+                    {
+                        FirstName = AuthorFirstNameTbx.Text,
+                        LastName = AuthorLastNameTbx.Text,
+                        BirthDate = isAuthorBirthDate
+                    }
+                );
+                db.SaveChanges();
+                LoadAuthors();
+            }
+        }
+
+
+        private void UpdateAuthorBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var db = new BookstoreContext();
+            SelectedAuthor = EditAuthorListView.SelectedItem as Author;
+
+            if (SelectedAuthor != null)
+            {
+
+            }
+
+        }
+
+        private void UpdateBookBtn_OnClick(object sender, RoutedEventArgs e)
+        {
 
         }
     }
--- Labb2/Views/StockView.xaml.cs
+++ Labb2/Views/StockView.xaml.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using DataAccess;
+using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Labb2.Views
 {
@@ -21,22 +12,144 @@ namespace Labb2.Views
     /// </summary>
     public partial class StockView : UserControl
     {
+        public Store? SelectedStore { get; set; }
+
+        public Stock? SelectedBookStore { get; set; }
+
+        public Book? SelectedBookStock { get; set; }
+
         public StockView()
         {
+           
             InitializeComponent();
             LoadBooks();
+
+            using var db = new BookstoreContext();
+
+            var stores = db.Stores.ToList();
+
+            foreach (var store in stores)
+            {
+                StoreCmb.Items.Add(store);
+            }
+            
 
         }
 
         public void LoadBooks()
         {
+            BookListView.Items.Clear();
             using var db = new BookstoreContext();
-
             var loadBooks = db.Books.ToList();
-
+            //BookListView.ItemsSource = loadBooks;
             foreach (var book in loadBooks)
             {
-                Store1ListView.Items.Add(book.Title);
+                BookListView.Items.Add(book);
+            }
+
+        }
+
+        public void LoadBooksByStore()
+        {
+
+            Store1ListView.Items.Clear();
+            using var db = new BookstoreContext();
+            var stocksForStore = db.Stocks
+                .Include(s => s.Store)
+                .Include(s => s.Isbn13Navigation)
+                .Where(s => s.StoreId == SelectedStore.Id);
+
+
+            foreach (var book in stocksForStore)
+            {
+                Store1ListView.Items.Add(book);
+            }
+
+        }
+
+
+        private void StoreCmb_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedStore = StoreCmb.SelectedItem as Store;
+            LoadBooksByStore();
+
+            //if (SelectedStore != null)
+            //{
+            //    Console.WriteLine($"SelectedStore: {SelectedStore.StoreName}");
+            //    LoadBooksByStore();
+            //}
+            //else
+            //{
+            //    // Log or debug the issue
+            //    Console.WriteLine("SelectedStore is null");
+            //}
+
+            //Store1ListView.Items.Clear();
+
+            //if (StoreCmb.SelectedItem is Store selectedStore)
+            //{
+            //    SelectedStore = selectedStore;
+            //    LoadBooksByStore();
+            //}
+            //else
+            //{
+            //    SelectedStore = null;
+            //    Store1ListView.Items.Clear(); 
+            //}
+        }
+
+        private void BookListView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedBookStock = BookListView.SelectedItem as Book;
+        }
+
+        private void RemoveBookBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var db = new BookstoreContext();
+            SelectedBookStore = Store1ListView.SelectedItem as Stock;
+
+            if (SelectedBookStore != null)
+            {
+                db.Stocks.Remove(SelectedBookStore);
+                db.SaveChanges();
+                LoadBooksByStore();
+            }
+        }
+
+        private void AddBookBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            using var db = new BookstoreContext();
+
+            if (int.TryParse(NumberOfBooksTbx.Text, out var numberOfBooksTrue) && SelectedBookStock != null && SelectedStore != null)
+            {
+                var storeById = db.Stocks
+                    .Where(s => s.StoreId == SelectedStore.Id);
+
+                if (storeById.Any(s => s.Isbn13 == SelectedBookStock.Isbn13))
+                {
+                    var bookExists = db.Stocks.FirstOrDefault(b =>
+                        b.StoreId == SelectedStore.Id && b.Isbn13 == SelectedBookStock.Isbn13);
+                    if (bookExists != null)
+                    {
+                        bookExists.Balance += numberOfBooksTrue;
+                        db.SaveChanges();
+                        LoadBooksByStore();
+                    }
+                }
+                else
+                {
+                    db.Stocks.Add
+                    (
+                        new Stock()
+                        {
+                            Isbn13 = SelectedBookStock.Isbn13,
+                            StoreId = SelectedStore.Id,
+                            Balance = int.Parse(NumberOfBooksTbx.Text)
+                        }
+                    );
+                    db.SaveChanges();
+                    LoadBooksByStore();
+                }
             }
         }
     }
